29acf2c53361b7ee959c6e036ed08fc86f091ecd
lkintheend <lkintheend@gmail.com>
2018-03-07 10:55:34 +0700

    fist

---
+++ Ailatrieuphu/src/Ailatrieuphu/Testclass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ailatrieuphu;
+
+/**
+ *
+ * @author Admin
+ */
+public class Testclass {
+    public static void main(String[] args) {
+        System.out.println(new Cauhoi(16).getcauhoi());
+    }
+}
